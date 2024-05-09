272f9d474dda0ee42a2cb9d9b006e3dbdadb304e
EgorNikitin1 <90258854+EgorNikitin1@users.noreply.github.com>
2024-05-10 01:59:38 +0300

    Davidon-Fletcher-Powell v0.3

--- Optimization/DavidonFletcherPowell.py
+++ Optimization/DavidonFletcherPowell.py
@@ -6,16 +6,18 @@ from sympy import diff, symbols, sqrt
 
 
 def derivative(func, x):
+    """Поиск производной в точке"""
     x1, x2 = symbols('x1 x2')
     return diff(func(x1, x2), x1).subs({x1: x[0], x2: x[1]}), diff(func(x1, x2), x2).subs({x1: x[0], x2: x[1]})
 
 
 def norm(x):
+    """Нормирование точки"""
     return sqrt(x[0] ** 2 + x[1] ** 2)
 
 
 def dfp(f, x0=np.array([0, 0]), err1=0.1, err2=0.15, M=10):
-
+    """Метод Дэвидона-Флетчера-Пауэлла"""
     k = 0
     A = np.eye(2)
     xp = x0
@@ -60,7 +62,7 @@ def dfp(f, x0=np.array([0, 0]), err1=0.1, err2=0.15, M=10):
 
 
 def main():
-    function = lambda x1, x2: 2 * x1 ** 2 + x1 * x2 + x2 ** 2
+    function = lambda x1, x2: 2 * x1 ** 2 + x1 * x2 + x2 ** 2  # Функция
     dfp(function, np.array([0.5, 1]))
 
 
