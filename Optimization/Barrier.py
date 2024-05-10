8e40ecf3de6d8aee6da79a138ba7228fd391ef68
EgorNikitin1 <90258854+EgorNikitin1@users.noreply.github.com>
2024-05-10 03:56:24 +0300

    Barrier v0.2

--- Optimization/Barrier.py
+++ Optimization/Barrier.py
@@ -11,35 +11,34 @@ def derivative(func1, func2):
     return diff(func1(x), x) + diff(func2(x, r), x)
 
 
-# F = lambda x, r: x ** 2 - 4 * x - r * sp.ln(1 - x)
-# F = lambda x1, x2, r: (x1 - 4) ** 2 + (x2 - 4) ** 2 - r * sp.ln(5 - x1 - x2)
-# F = lambda x1, x2, r: x1 ** 2 + x2 ** 2 + 6 * x1 - 2 * x2 - r * sp.ln(1 - x1 ** 2 - x2 ** 2)
+def barrier(x0=np.array([0.0, 0.0], float), err=0.01, C=10):
+    F = lambda x1, x2: -2 * x1 - 4 * x2 + x1 ** 2 + x2 ** 2 - r * sp.ln(1 - x1 - x2)
 
-F = lambda x1, x2: -2 * x1 - 4 * x2 + x1 ** 2 + x2 ** 2 - r * sp.ln(1 - x1 - x2)
+    r = 1
+    k = 0
+    x = x0
+    while True:
+        print(f"Barrier k = {k}")
+        xt = dfp(F, x)
 
-x0 = np.array([1, 0.5])
-err = 0.01
-C = 10
-r = 1
-k = 0
-x = x0
-while True:
-    print(f"Barrier k = {k}")
-    print()
-    xt = dfp(F, x)
+        if xt[0] + xt[1] <= 1:
+            f = -2 * xt[0] - 4 * xt[1] + xt[0] ** 2 + xt[1] ** 2
+            P = - r * sp.ln(1 - xt[0] - xt[1])
+            if abs(P) <= err:
+                print("Minimum is found:")
+                print(f"x = {xt.round(5)}")
+                print(f"f(x) = {round(f, 5)}")
+                break
+        print(f"r = {r}\tx = {x.round(5)}\tF(x) = {round(F(*xt), 5)}\tP(x) = {round(abs(P), 5)}\tf(x) = {round(f, 5)}\t")
+        r = r / C
+        x = xt
+        k += 1
+        print()
 
-    if xt[0] + xt[1] <= 1:
-        print(f"F(x) = {F(*xt)}")
-        f = -2 * xt[0] - 4 * xt[1] + xt[0] ** 2 + xt[1] ** 2
-        print(f"f(x) = {f}")
 
-        P = - r * sp.ln(1 - xt[0] - xt[1])
-        print(f"P(x) = {abs(P)}")
-        if abs(P) <= err:
-            print(f"x = {x}")
-            break
-    r = r / C
-    x = xt
-    k += 1
-    print(f"x = {x}")
-    print()
+def main():
+    barrier(np.array([1.0, 0.5], float), 0.01)
+
+
+if __name__ == '__main__':
+    main()
