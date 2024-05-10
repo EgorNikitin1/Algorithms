ef5f85a2af87fb9741daa87fb475b87b53962fb0
EgorNikitin1 <90258854+EgorNikitin1@users.noreply.github.com>
2024-05-10 03:29:47 +0300

    Davidon-Fletcher-Powell v0.4

--- Optimization/DavidonFletcherPowell.py
+++ Optimization/DavidonFletcherPowell.py
@@ -16,7 +16,7 @@ def norm(x):
     return sqrt(x[0] ** 2 + x[1] ** 2)
 
 
-def dfp(f, x0=np.array([0, 0]), err1=0.1, err2=0.15, M=10):
+def dfp(f, x0=np.array([0.0, 0.0], float), err1=0.1, err2=0.15, M=10):
     """Метод Дэвидона-Флетчера-Пауэлла"""
     k = 0
     A = np.eye(2)
@@ -24,20 +24,25 @@ def dfp(f, x0=np.array([0, 0]), err1=0.1, err2=0.15, M=10):
     At = A
     while True:
         print(f"k = {k}")
-        grad = np.array(derivative(f, xp))
-        print(f"Gradient = {grad}")
+        grad = np.array(derivative(f, xp), float)
         if norm(grad) < err1:
-            print(f"xt = {xt}")
+            print("Minimum is found:")
+            print(f"x = {xt.round(5)}")
+            print("f(x) = ", end='')
+            print(round(f(*xt), 5))
             break
         else:
             if k == 0:
                 pass
             elif k >= M:
-                print(f"xt = {xt}")
+                print("Minimum is found:")
+                print(f"x = {xt.round(5)}")
+                print("f(x) = ", end='')
+                print(f(*xt))
                 break
             else:
-                deltagrad = np.array([[(grad - np.array(derivative(f, xpp)))[0]], [(grad - np.array(derivative(f, xpp)))[1]]])
-                deltax = np.array([[(xp - xpp)[0]], [(xp - xpp)[1]]])
+                deltagrad = np.array([[(grad - np.array(derivative(f, xpp)))[0]], [(grad - np.array(derivative(f, xpp)))[1]]], float)
+                deltax = np.array([[(xp - xpp)[0]], [(xp - xpp)[1]]], float)
 
                 Ac = deltax @ deltax.T / (deltax.T @ deltagrad) - (A @ deltagrad @ deltagrad.T @ A) / (deltagrad.T @ A @ deltagrad)
                 At = A + Ac
@@ -46,11 +51,15 @@ def dfp(f, x0=np.array([0, 0]), err1=0.1, err2=0.15, M=10):
             t = symbols('t')
             f_aug = f(*(xp + t * d))
             t = sp.solve(sp.Eq(diff(f_aug, t), 0))[0]
-            print(f"t = {t}")
             xt = xp + t * d
-            print(f"xt = {xt}")
+            xt = np.array(xt, float)
+            print(f"grad_f = {grad.round(5)}\tt = {round(t, 5)}\td = {d.round(5)}\tx = {xt.round(5)}")
 
             if norm(xt - xp) < err2 and abs(f(*xt) - f(*xp)) < err2:
+                print("Minimum is found:")
+                print(f"x = {xt.round(5)}")
+                print("f(x) = ", end='')
+                print(f(*xt))
                 break
             else:
                 k += 1
@@ -63,7 +72,7 @@ def dfp(f, x0=np.array([0, 0]), err1=0.1, err2=0.15, M=10):
 
 def main():
     function = lambda x1, x2: 2 * x1 ** 2 + x1 * x2 + x2 ** 2  # Функция
-    dfp(function, np.array([0.5, 1]))
+    dfp(function, np.array([0.5, 1.0], float))
 
 
 if __name__ == '__main__':
